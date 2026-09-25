31a72126ae599b3238ccffecc3f984dfcbf6a073
raditted <radithya.syabil@gmail.com>
2026-09-25 21:08:55 +0700

    Add menu items and availability checks in Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,13 @@ public class Main {
 
         System.out.println(m1.getNim() + " - " + m1.getNama());
         System.out.println(m2.getNim() + " - " + m2.getNama());
+
+        MenuItem nasi = new MenuItem("M01", "Nasi Goreng", 18000);
+        MenuItem kopi = new MenuItem("M02", "Kopi Susu", 12000);
+
+        kopi.tandaiHabis();
+
+        System.out.println(nasi.getNama() + ": " + nasi.isTersedia());
+        System.out.println(kopi.getNama() + ": " + kopi.isTersedia());
     }
 }
