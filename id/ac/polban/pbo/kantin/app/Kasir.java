0ba58d7fc3b79edee6335a1b3107d78593501af9
raditted <radithya.syabil@gmail.com>
2026-09-26 12:24:35 +0700

    Update Kasir class comments to clarify relationship with Pesanan

--- id/ac/polban/pbo/kantin/app/Kasir.java
+++ id/ac/polban/pbo/kantin/app/Kasir.java
@@ -2,7 +2,8 @@ package id.ac.polban.pbo.kantin.app;
 import id.ac.polban.pbo.kantin.model.Pesanan;
 
 public class Kasir {
-    // Relasi uses-a: Pesanan hanya diterima sebagai parameter method
+    // Challange A
+    // Relasi uses-a, Pesanan hanya diterima sebagai parameter method. artinya yang berinteraksi dengan Pesanan hanya ketika method dijalankan,
     public void proses(Pesanan pesanan) {
         System.out.println(pesanan.getNomor());
         if (pesanan.dapatDiproses()) {
