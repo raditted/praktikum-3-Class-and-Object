84d515774b92fb165c8684091ffa50bea43c0149
raditted <radithya.syabil@gmail.com>
2026-09-26 12:19:07 +0700

    Add Kasir class for order processing and remove unused classes

---
+++ id/ac/polban/pbo/kantin/app/Kasir.java
@@ -0,0 +1,14 @@
+package id.ac.polban.pbo.kantin.app;
+import id.ac.polban.pbo.kantin.model.Pesanan;
+
+public class Kasir {
+    // Relasi uses-a: Pesanan hanya diterima sebagai parameter method
+    public void proses(Pesanan pesanan) {
+        System.out.println(pesanan.getNomor());
+        if (pesanan.dapatDiproses()) {
+            System.out.println("Pesanan diterima. Rp " + pesanan.hitungTotal());
+        } else {
+            System.out.println("Pesanan ditolak");
+        }
+    }
+}
